5b2a19b72b21d69cfe8ec4aa320560a689a9c787
henrique-caldeira <55319195+henrique-caldeira@users.noreply.github.com>
2019-10-17 19:56:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
-<?php $this->layout('template', ['title' => 'User Profile']) ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <style>
+     @import url('https://fonts.googleapis.com/css?family=Varela+Round&display=swap');
+    </style>               
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Cupom</title>
+<head>
+    <body>
 
-<h1>Welcome!</h1>
-<p>Hello <?=$this->e($name)?></p>
+    <section id="side-bar">
+        <div>
+            <h1 id="cupom">CUP%M</h1>
+        </div>
 
-<h2>Friends</h2>
-<ul>
-    <?php foreach($friends as $friend): ?>
-        <li>
-            <a href="/profile/<?=$this->e($friend->id)?>">
-                <?=$this->e($friend->name)?>
-            </a>
-        </li>
-    <?php endforeach ?>
-</ul>
-
-<?php if ($invitations): ?>
-    <h2>Invitations</h2>
-    <p>You have some friend invites!</p>
-<?php endif ?>
+        <div>
+            <ul class="lista-menu">
+                <li class="item-menu"><a href=""><img src="imagens/home.png" class="homelogo">Home</a></li>
+                <li class="item-menu"><a href=""><img src="imagens/hotlogo.png" class="hotlogo-menu">Mais Rentaveis</a></li>
+                <li class="item-menu"><a href="">Publicam Cupom</a></li>
+                <li class="item-menu"><a href="">Pesquisar</a></li>
+            </ul>
+        </div>
+    </body>
+</head>
